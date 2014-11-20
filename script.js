3a8300dd332666425acd063b4753ba80e2b776af
Alfau <alfauadam.16@gmail.com>
2014-11-21 03:58:11 +0500

    mobile layout for homepage done

--- script.js
+++ script.js
@@ -1,56 +1,26 @@
 $(document).ready(function(){
 	$(document).on("click","a#menu",function(){
 		if($(this).hasClass("active")){
-			$(this).children().attr("class","");
-			load_remove_individually("remove");
-			$(this).removeClass("active");
+			$(this).removeClass("active").children("svg").attr("class","");
+			$("header#large div#header_bottom").slideUp();
 		}else{
-			$(this).children("svg").attr("class","active");
-			load_remove_individually('load');
-			$(this).addClass("active");
+			$(this).addClass("active").children("svg").attr("class","active");
+			$("header#large div#header_bottom").slideDown();
 		}
 	});
 	
-	// $(window).scroll(function(){
-		// if($(window).scrollTop()>300){
-			// $("header#small").slideDown(200);
-		// }else if($(window).scrollTop()<=300){
-			// $("header#small").slideUp("fast");
-		// }
-	// });
+	if(window.innerHeight>=984){
+		$(window).scroll(function(){
+			if($(window).scrollTop()>300){
+				$("header#small").slideDown(200);
+			}else if($(window).scrollTop()<=300){
+				$("header#small").slideUp("fast");
+			}
+		});
+	}
 	
 	slideshow(9000,700);
 });
-
-function load_remove_individually(request){
-	var list="div#header_bottom li";
-	var count=$(list).length;
-	var interval=150;
-	
-	if(request=="load"){
-		var increment=1;
-		$(list+":eq(0)").stop().slideDown(interval);
-		var Interval=setInterval(function(){
-			if(increment!=count){
-				$(list+":eq("+increment+")").stop().slideDown(interval);
-				increment++;
-			}else if(increment>=count){
-				clearInterval(Interval);
-			}
-		},interval);
-	}else if(request=="remove"){
-		var increment=count-1;
-		$(list+":eq("+count+")").stop().slideUp(interval);
-		var Interval=setInterval(function(){
-			if(increment<=count){
-				$(list+":eq("+increment+")").stop().slideUp(interval);
-				increment--;
-			}else if(increment<=0){
-				clearInterval(Interval);
-			}
-		},interval);
-	}
-}
 
 
 function slideshow(interval,speed){
